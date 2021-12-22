3d4d77cece36732d437fd2ba68ac5c128cdb8757
Alex Brandt <brandta7@msu.edu>
2021-12-22 01:49:41 -0500

    Create route for getting my posts

--- server/routes/post.js
+++ server/routes/post.js
@@ -38,4 +38,16 @@ router.post('/createpost', requireLogin, (req, res)=>{
     })
 })
 
+router.get('/myposts', requireLogin, (req, res)=>{
+
+    Post.find({postedBy:req.user._id})
+        .populate("postedBy", "_id name")
+        .then(mypost=>{
+            res.json({mypost})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+})
+
 module.exports = router
