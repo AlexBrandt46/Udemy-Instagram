0d47f07b1d0692226d7bc3d468ecb573fe6f8d32
Alex Brandt <brandta7@msu.edu>
2022-01-03 15:35:04 -0500

    Created signin and signup page UI

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,9 +1,21 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
-const Signup = ()=>{
+const Login = ()=>{
     return (
-        <h1>Sign Up</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="name" />
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
+                <button class="btn waves-effect waves-light #64b5f6 blue lighten-2">Sign Up</button>
+                <h5>
+                    <Link to="/login">Already Have an Account?</Link>
+                </h5>
+            </div>
+        </div>
     )
 }
 
-export default Signup
+export default Login
