31f79ee83cf6b0635cc222e45aa2e0a16d042fe7
Alex Brandt <brandta7@msu.edu>
2022-01-04 10:53:52 -0500

    Created initial UI for profile page

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -2,7 +2,39 @@ import React from "react";
 
 const Profile = ()=>{
     return (
-        <h1>Profile</h1>
+        <div style={{maxWidth: "500px", margin: "0px auto"}}>
+            <div id="" style={{
+                display:"flex",
+                justifyContent:"space-around",
+                margin:"18px 0px",
+                borderBottom:"1px solid grey"
+            }}>
+                <div>
+                    <img 
+                        style={{width: "160px", height:"160px", borderRadius:"80px"}}
+                        src="https://images.unsplash.com/photo-1475692277358-d66444784d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                    />
+                </div>
+                <div>
+                    <h4>Alex Brandt</h4>
+                    <div style={{display:"flex", justifyContent: "space-between", width:"108%"
+
+                    }}>
+                        <h6>40 Posts</h6>
+                        <h6>40 Followers</h6>
+                        <h6>40 Following</h6>
+                    </div>
+                </div>
+            </div>
+            <div className="gallery">
+                <img className="item" src="https://images.unsplash.com/photo-1475692277358-d66444784d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1475692277358-d66444784d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1475692277358-d66444784d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1475692277358-d66444784d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1475692277358-d66444784d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item" src="https://images.unsplash.com/photo-1475692277358-d66444784d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+            </div>
+        </div>
     )
 }
 
