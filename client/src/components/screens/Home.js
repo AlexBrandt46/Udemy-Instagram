540cb83958ad6e07e8082d730d79261d674cafc1
Alex Brandt <brandta7@msu.edu>
2022-01-04 11:05:39 -0500

    Created static home page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,58 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div>
+                <div className="card home-card">
+                    <h5>Alex</h5>
+                    <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                    </div>
+                    <div className="card-content">
+                        <i class="material-icons" style={{color: "red"}}>favorite</i>
+                        <h6>Title</h6>
+                        <p>This is an amazing post</p>
+                        <input type="text" placeholder="Add a comment" />
+                    </div>
+                </div>
+                <div className="card home-card">
+                    <h5>Alex</h5>
+                    <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                    </div>
+                    <div className="card-content">
+                        <i class="material-icons" style={{color: "red"}}>favorite</i>
+                        <h6>Title</h6>
+                        <p>This is an amazing post</p>
+                        <input type="text" placeholder="Add a comment" />
+                    </div>
+                </div>
+                <div className="card home-card">
+                    <h5>Alex</h5>
+                    <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                    </div>
+                    <div className="card-content">
+                        <i class="material-icons" style={{color: "red"}}>favorite</i>
+                        <h6>Title</h6>
+                        <p>This is an amazing post</p>
+                        <input type="text" placeholder="Add a comment" />
+                    </div>
+                </div>
+                <div className="card home-card">
+                    <h5>Alex</h5>
+                    <div className="card-image">
+                        <img src="https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2FsbHBhcGVyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                    </div>
+                    <div className="card-content">
+                        <i class="material-icons" style={{color: "red"}}>favorite</i>
+                        <h6>Title</h6>
+                        <p>This is an amazing post</p>
+                        <input type="text" placeholder="Add a comment" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
