b48f1a7403fccb9854305a387a34b0f9aa8db8a0
Alex Brandt <ajb3155@gmail.com>
2022-09-04 15:27:52 -0400

    Moved client folder

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
-const {MONGOURI} = require('./keys')
+const { MONGOURI } = require('./keys')
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
