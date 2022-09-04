f4affb5db000ee9560bd0225a15941558a0593dc
Alex Brandt <ajb3155@gmail.com>
2022-09-04 15:56:59 -0400

    Start to deploy

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
 
-const { MONGOURI } = require('./keys')
+const { MONGOURI } = require('./config/keys')
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
@@ -28,6 +28,10 @@ app.use(require('./routes/user'))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 app.listen(PORT, ()=>{
